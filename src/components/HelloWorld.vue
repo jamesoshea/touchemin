bed211280af309a1d13214ad7cd575cbc0ef9dd1
James O'Shea <jamesoshea89@hotmail.com>
2018-04-22 20:28:24 +0200

    basic linting setup done

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+const wowdlksdlkn = 'poo';
+console.error(wowdlksdlkn);
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
