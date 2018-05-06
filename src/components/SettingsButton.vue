ea2aa6a41b1e1306207ab7047ee46cd3b0ddae9e
James O'Shea <jo@3yourmind.com>
2018-05-06 15:13:40 +0200

    settings button added

---
+++ src/components/SettingsButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="touchemin__settings-icon-container">
+    <SettingsSVG/>
+  </div>
+</template>
+
+<script>
+import SettingsSVG from '../assets/svgIcons/settingsSVG.vue';
+export default {
+  components: {
+    SettingsSVG,
+  },
+  methods: {
+    openSettingsModal() {},
+  },
+};
+</script>
+
+<style lang="scss">
+.touchemin__settings-icon-container {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  height: 15vh;
+  width: 15vh;
+}
+</style>
