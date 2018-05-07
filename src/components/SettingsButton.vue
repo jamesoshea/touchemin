f2eec0a7e1d3a8a567110149f905a026de2105dc
James O'Shea <jo@3yourmind.com>
2018-05-07 21:02:23 +0200

    add modal for settings

--- src/components/SettingsButton.vue
+++ src/components/SettingsButton.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="touchemin__settings-icon-container">
-    <SettingsSVG/>
+    <div @click="openSettingsModal">
+      <SettingsSVG @click="openSettingsModal"/>
+    </div>
+    <ToucheminModal v-if="showModal" @closeModal="closeSettingsModal">
+      <h1 slot="title" v-text="'Settings'" />
+      <div slot="body">
+        wow
+      </div>
+    </ToucheminModal>
   </div>
 </template>
 
 <script>
 import SettingsSVG from '../assets/svgIcons/settingsSVG.vue';
+import ToucheminModal from './ToucheminModal.vue';
 export default {
+  name: 'SettingsButton',
   components: {
     SettingsSVG,
+    ToucheminModal,
+  },
+  data() {
+    return {
+      showModal: false,
+    }
   },
   methods: {
-    openSettingsModal() {},
+    openSettingsModal() {
+      console.log('hello')
+      this.showModal = true;
+    },
+    closeSettingsModal() {
+      this.showModal = false;
+    },
   },
 };
 </script>
